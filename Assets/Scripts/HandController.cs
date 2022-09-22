8519d3759344b5bb474a96560faa6d93665f0b6b
Reece <allus.interactive@gmail.com>
2022-09-22 20:40:46 +0100

    started work on hand logic

---
+++ Assets/Scripts/HandController.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HandController : MonoBehaviour
+{
+    public Card[] heldCards;
+
+    public Transform minPosition;
+    public Transform maxPosition;
+
+    void Start()
+    {
+        SetCardPositionsInHand();
+    }
+
+    void Update()
+    {
+        
+    }
+
+    public void SetCardPositionsInHand()
+    {
+        
+    }
+}
