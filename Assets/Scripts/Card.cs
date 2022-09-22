91af9ce9f850ac64d8ad6983ea9c4c6cdd6e97a7
Reece <allus.interactive@gmail.com>
2022-09-22 21:27:14 +0100

    added logic to animate card movement

--- Assets/Scripts/Card.cs
+++ Assets/Scripts/Card.cs
@@ -22,16 +22,21 @@ public class Card : MonoBehaviour
     public Image characterArt;
     public Image backgroundArt;
 
-    // Start is called before the first frame update
+    public float moveSpeed = 5f;
+    public float rotateSpeed = 540f;
+
+    private Vector3 targetPoint;
+    private Quaternion targetRotation;
+
     void Start()
     {
         InitializeCard();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        transform.position = Vector3.Lerp(transform.position, targetPoint, (moveSpeed * Time.deltaTime));
+        transform.rotation = Quaternion.RotateTowards( transform.rotation, targetRotation, (rotateSpeed * Time.deltaTime));
     }
 
     public void InitializeCard()
@@ -50,5 +55,11 @@ public class Card : MonoBehaviour
 
         characterArt.sprite = CardAsset.cardSprite;
         backgroundArt.sprite = CardAsset.backgroundSprite;
+    }
+
+    public void MoveToPoint(Vector3 destination, Quaternion rotation)
+    {
+        targetPoint = destination;
+        targetRotation = rotation;
     }
 }
