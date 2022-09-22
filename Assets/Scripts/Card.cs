a1177b2de1426908c92203b391c3f80729fd24e7
Reece <allus.interactive@gmail.com>
2022-09-22 20:28:11 +0100

    load data from scriptable object

--- Assets/Scripts/Card.cs
+++ Assets/Scripts/Card.cs
@@ -2,9 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using UnityEngine.UI;
 
 public class Card : MonoBehaviour
 {
+    public CardScriptableObject CardAsset;
+
     public int currentHealth;
     public int attackPower;
     public int manaCost;
@@ -12,18 +15,40 @@ public class Card : MonoBehaviour
     public TMP_Text healthText;
     public TMP_Text attackText;
     public TMP_Text manaText;
+    public TMP_Text nameText;
+    public TMP_Text actionText;
+    public TMP_Text descriptionText;
+
+    public Image characterArt;
+    public Image backgroundArt;
 
     // Start is called before the first frame update
     void Start()
     {
-        healthText.text = currentHealth.ToString();
-        attackText.text = attackPower.ToString();
-        manaText.text = manaCost.ToString();
+        InitializeCard();
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    public void InitializeCard()
+    {
+        currentHealth = CardAsset.currentHealth;
+        attackPower = CardAsset.attackPower;
+        manaCost = CardAsset.manaCost;
+
+        healthText.text = currentHealth.ToString();
+        attackText.text = attackPower.ToString();
+        manaText.text = manaCost.ToString();
+
+        nameText.text = CardAsset.cardName;
+        actionText.text = CardAsset.actionDescription;
+        descriptionText.text = CardAsset.cardDescription;
+
+        characterArt.sprite = CardAsset.cardSprite;
+        backgroundArt.sprite = CardAsset.backgroundSprite;
     }
 }
