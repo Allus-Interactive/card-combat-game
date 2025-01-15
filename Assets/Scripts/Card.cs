d7da4071eea508fe7243f9aefdd3c90673435478
Reece <reecemorgandev@gmail.com>
2025-01-15 11:18:53 +0000

    card scriptable object

--- Assets/Scripts/Card.cs
+++ Assets/Scripts/Card.cs
@@ -1,25 +1,51 @@
-using UnityEngine;
 using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
 
 public class Card : MonoBehaviour
 {
-    public int currentHealth;
-    public int attackPower;
-    public int manaCost;
+    public CardScriptableObject cardSO;
 
     public TMP_Text healthText;
     public TMP_Text attackText;
     public TMP_Text costText;
 
+    public TMP_Text cardNameText;
+    public TMP_Text actionDescriptionText;
+    public TMP_Text cardLoreText;
+
+    public Image characterArt;
+    public Image backgroundArt;
+
+    private int currentHealth;
+    private int attackPower;
+    private int manaCost;
+
     void Start()
     {
-        healthText.text = currentHealth.ToString();
-        attackText.text = attackPower.ToString(); 
-        costText.text = manaCost.ToString();
+        setUpCard();
     }
 
     void Update()
     {
         
+    }
+
+    public void setUpCard()
+    {
+        currentHealth = cardSO.currentHealth;
+        attackPower = cardSO.attackPower;
+        manaCost = cardSO.manaCost;
+
+        healthText.text = currentHealth.ToString();
+        attackText.text = attackPower.ToString();
+        costText.text = manaCost.ToString();
+
+        cardNameText.text = cardSO.cardName;
+        actionDescriptionText.text = cardSO.actionDescription;
+        cardLoreText.text = cardSO.cardLore;
+
+        characterArt.sprite = cardSO.characterSprite;
+        backgroundArt.sprite = cardSO.backgroundSprite;
     }
 }
